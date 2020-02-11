34042d52d5a7c9b072126769007cc6f957a5482d
SheebaJ <ashasheebajnn1996@gmail.com>
2020-02-11 09:10:24 +0530

    changes in phase1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,11 +6,12 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
     def create
+        #binding.pry
         @article = Article.new
         @article.title = params['article']['title']
         @article.body = params['article']['body']
         @article.category_id = params['article']['category_id']
-        @article.published_date = params['article']['published_date']
+        @article.publish_date = params['article']['publish_date']
         if @article.publish_date <= Date.today
 			@article.is_published = true
 		else
@@ -33,7 +34,7 @@ class ArticlesController < ApplicationController
         @article.title = params["article"]["title"]
         @article.body = params["article"]["body"]
         @article.category_id = params['article']['category_id']
-        @article.published_date = params['article']['published_date']
+        @article.publish_date = params['article']['publish_date']
         @article.feature_image_url = params['article']['feature_image_url']
         if @article.publish_date <= Date.today
 			@article.is_published = true
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,11 @@
 class Article < ApplicationRecord
     belongs_to :category
     validates_presence_of :title,:body,:category_id,:publish_date,:feature_image_url
-    validate :article_published
+    validate :article_published?
 
-    def article_published
+    def article_published?
         if publish_date > 1.month.from_now
-			errors.add(:publish_date,"date greater than a month")
+		  errors.add(:publish_date,"date greater than a month")
 		end
     end
 end
