8f91c3bf8ddb6072be80e5b23f3e45e1b89cf84e
SheebaJ <ashasheebajnn1996@gmail.com>
2020-02-11 11:13:08 +0530

    Changes

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-    has_many :articles
+    has_many :articles, dependent: :destroy
 
     validates_presence_of :name, :description
 	validates_uniqueness_of :name
