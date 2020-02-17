58a3c4fb303ccb949d4ce119c9cceefec78df9ad
SheebaJ <ashasheebajnn1996@gmail.com>
2020-02-17 10:15:16 +0530

    fix: role model changed name->title

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,7 +2,7 @@ class Role < ApplicationRecord
     has_many :permissions
     has_many :users, through: :permissions 
  
-    validates_presence_of :name
-    validates_uniqueness_of :name
+    validates_presence_of :title
+    validates_uniqueness_of :title
 
 end
